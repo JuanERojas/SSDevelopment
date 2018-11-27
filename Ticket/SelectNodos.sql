dbb7d6e04cba7c68e1b8fbbe21a6885a52459a98
JuanERojas <juan.rojas14@hotmail.com>
2018-11-27 17:26:39 -0300

    JR-Actualizacion Tk
    stats4.php Estadisticas por Nodo. Pero con bugs.
    index2.php Reclamos dinamicos.

--- Ticket/SelectNodos.sql
+++ Ticket/SelectNodos.sql
@@ -1,6 +1,17 @@
-﻿SELECT *
+﻿SELECT dreclamo, t.nombre AS tecnico, nodo, problema, numcliente, `time`
 FROM `SS-DBTK`.reclamo r
+INNER JOIN `SS-DBTK`.tecnico AS t ON r.tecnico = t.idtecnico
 INNER JOIN `SS-DB`.panel AS p ON r.nodo = p.nombre
 INNER JOIN `SS-DB`.nodo AS n ON p.nodo = n.idnodo
 WHERE n.nombre LIKE 'FTTH'
 LIMIT 10;
+
+
+SELECT idreclamo, t.nombre AS tecnico, nodo, problema, numcliente, time FROM reclamo r INNER JOIN tecnico AS t ON r.tecnico = t.idtecnico WHERE `time` > DATE_SUB(NOW(), INTERVAL '3:0:".$intervalo."' HOUR_SECOND) AND r.nodo LIKE '".$nodo."'  ORDER BY ".$orden.";";
+
+SELECT r.idreclamo, t.nombre AS tecnico, r.nodo, r.problema, r.numcliente, r.time FROM `SS-DBTK`.reclamo r
+			INNER JOIN `SS-DBTK`.tecnico AS t ON r.tecnico = t.idtecnico 
+			INNER JOIN `SS-DB`.panel AS p ON r.nodo = p.nombre
+			INNER JOIN `SS-DB`.nodo AS n ON p.nodo = n.idnodo
+			WHERE n.nombre LIKE 'FTTH'
+			ORDER BY "idreclamo";
